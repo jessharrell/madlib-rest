f0c073ba03e36564a81f5d570a8a8c64d09e334c
Jess Harrell <jharrell@pillartechnology.com>
2018-12-22 16:05:21 -0600

    cleanup

--- specs/puzzle.get.spec.js
+++ specs/puzzle.get.spec.js
@@ -5,50 +5,40 @@ var serverConfig = require("./test.server.config")
 
 describe("get - /puzzle", function () {
     describe("requesting specific puzzle", function() {
-        it("returns puzzle when puzzle is known", function (done) {
-            var testID = uuid4();
+        beforeEach(function() {
             if(!fs.existsSync(serverConfig.PuzzleLocation)){
                 fs.mkdirSync(serverConfig.PuzzleLocation);
             }
+        });
+
+        afterEach(function () {
+            fs.removeSync(serverConfig.PuzzleLocation);
+        });
+
+        it("returns puzzle when puzzle is known", function async(done) {
+            var testID = uuid4();
             fs.writeFileSync(serverConfig.PuzzleLocation + "/" + testID, "Puzzle Content")
 
             axios.get("http://localhost:3000/" + "puzzles/" + testID)
                 .then(function (response) {
-                    try {
-                        expect(response.data).toEqual("Puzzle Content");
-                    } catch (error) {
-                        fs.removeSync(serverConfig.PuzzleLocation);
-                        done.fail(error);
-                    }
-                    fs.removeSync(serverConfig.PuzzleLocation);
+                    expect(response.data).toEqual("Puzzle Content");
                     done();
                 })
                 .catch(function (error) {
-                    fs.removeSync(serverConfig.PuzzleLocation);
                     done.fail(error);
                 });
         });
 
-        it("returns different puzzle when puzzle is known", function (done) {
+        it("returns different puzzle when puzzle is known", function async(done) {
             var testID = uuid4();
-            if(!fs.existsSync(serverConfig.PuzzleLocation)){
-                fs.mkdirSync(serverConfig.PuzzleLocation);
-            }
             fs.writeFileSync(serverConfig.PuzzleLocation + "/" + testID, "Different Puzzle Content")
 
             axios.get("http://localhost:3000/" + "puzzles/" + testID)
                 .then(function (response) {
-                    try {
-                        expect(response.data).toEqual("Different Puzzle Content");
-                    } catch (error) {
-                        fs.removeSync(serverConfig.PuzzleLocation);
-                        done.fail(error);
-                    }
-                    fs.removeSync(serverConfig.PuzzleLocation);
+                    expect(response.data).toEqual("Different Puzzle Content");
                     done();
                 })
                 .catch(function (error) {
-                    fs.removeSync(serverConfig.PuzzleLocation);
                     done.fail(error);
                 });
         });
