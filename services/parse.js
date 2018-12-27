e81f9eccad0336634fcbfd867846b36af42c599f
Jess Harrell <jharrell@pillartechnology.com>
2018-12-27 10:07:51 -0600

    refactor

--- services/parse.js
+++ services/parse.js
@@ -3,13 +3,13 @@ module.exports = function(text) {
     if (indexOfDynamic < 0){
         return [{type: "static", text: text}];
     } else {
-        if (indexOfDynamic != 0) {
+        if (indexOfDynamic !== 0) {
             return [{ type : 'static', text : text.substr(0 , indexOfDynamic - 1)},
                 { type : text.substr(indexOfDynamic+1), text : '' }]
         }
 
         var firstSpace = text.indexOf(" ");
-        return [ { type : text.substr(0, firstSpace).substr(1), text : '' },
+        return [ { type : text.substr(indexOfDynamic, firstSpace).substr(1), text : '' },
                  { type : 'static', text : text.substr(firstSpace + 1)} ]
     }
 
