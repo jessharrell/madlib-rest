ef2626e91d873f4afd95d2c308d338fd9a12f73d
Jess Harrell <jharrell@pillartechnology.com>
2018-12-28 11:47:09 -0600

    refactor for readability

--- services/parse.js
+++ services/parse.js
@@ -3,25 +3,46 @@ module.exports = function(text) {
     if (indexOfDynamic < 0){
         return [{type: 'static', text: text}];
     } else {
-        var list = []
-        var restOfString = text;
-        var currentIndex = 0;
-        while (indexOfDynamic >= 0) {
-            var indexOfNextSpace = restOfString.substr(indexOfDynamic + 1).indexOf(" ");
-            if (indexOfNextSpace < 0) {
-                indexOfNextSpace = text.length - currentIndex;
-            }
-            list.push({type: 'static', text: restOfString.substr(0, indexOfDynamic - 1)});
-            list.push({type: restOfString.substr(indexOfDynamic + 1, indexOfNextSpace), text: ''});
-            currentIndex =  indexOfDynamic + indexOfNextSpace + 1;
-            restOfString = restOfString.substr(currentIndex + 1);
-            indexOfDynamic = restOfString.indexOf('_');
-        }
-
-        if(currentIndex < text.length) {
-            list.push({type: 'static', text: restOfString});
-        }
-
-        return list.filter(function (element) { return element.type !== 'static' || element.text.trim() !== ''});
+        var list = parseStringWithDynamics(text)
+        return removeBlankStaticTexts(list)
     }
 };
+
+
+function removeBlankStaticTexts(listOfPuzzlePieces) {
+    return listOfPuzzlePieces.filter(function (piece) { return piece.type !== 'static' || piece.text.trim() !== ''});
+}
+
+function findLengthOfDynamic(restOfString, indexOfDynamic) {
+    var startOfDynamicType = indexOfDynamic + 1;
+    var length = restOfString.substr(startOfDynamicType).indexOf(" ");
+    if (length < 0) {
+        length = restOfString.length - startOfDynamicType;
+    }
+    return length;
+}
+
+var space = 1;
+var underscore = 1;
+
+function parseStringWithDynamics(text) {
+    var indexOfDynamic = text.indexOf('_')
+    var list = [];
+    var restOfString = text;
+    var currentIndex = 0;
+    while (indexOfDynamic >= 0) {
+        var lengthOfDynamic = findLengthOfDynamic(restOfString, indexOfDynamic);
+
+        list.push({type: 'static', text: restOfString.substr(0, indexOfDynamic - space)});
+        list.push({type: restOfString.substr(indexOfDynamic + underscore, lengthOfDynamic), text: ''});
+
+        currentIndex =  indexOfDynamic + underscore + lengthOfDynamic + space;
+        restOfString = restOfString.substr(currentIndex);
+        indexOfDynamic = restOfString.indexOf('_');
+    }
+
+    if(currentIndex < text.length) {
+        list.push({type: 'static', text: restOfString});
+    }
+    return list
+}
